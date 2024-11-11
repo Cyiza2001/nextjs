586d22fb41c70fcf096c8b217c94f4927a74efa5
Cyiza2001 <82471629+Cyiza2001@users.noreply.github.com>
2024-11-11 23:34:22 +0200

    another commit

---
+++ nextapp/src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function loginPage() {
+  return (
+    <div className="flex items-center justify-center w-full h-full ">
+      <h1 className=" text-red-900 text-2xl">Login</h1>
+    </div>
+  );
+}
---
+++ nextapp/src/app/signup/page.tsx
@@ -0,0 +1,8 @@
+export default function signUpPage (){
+    return(
+        <div>
+            <h1>signUp</h1>
+        </div>
+    )
+
+}
