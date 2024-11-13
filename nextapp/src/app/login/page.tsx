6fc26ede2ef02c66d083103de435b0e7c079341a
Cyiza2001 <82471629+Cyiza2001@users.noreply.github.com>
2024-11-13 23:58:17 +0200

    another commit

--- nextapp/src/app/login/page.tsx
+++ nextapp/src/app/login/page.tsx
@@ -1,7 +1,56 @@
-export default function loginPage() {
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import { axios } from "axios";
+export default function signUpPage() {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+ 
+  });
+  const onLogIn = () => {};
   return (
-    <div className="flex items-center justify-center w-full h-full ">
-      <h1 className=" text-red-900 text-2xl">Login</h1>
+    <div className="flex flex-col justify-center items-center h-screen gap-3">
+        <div className=" bg-pink-600 h-96 w-80 px-3 flex flex-col gap-2">
+        <h1 className="flex justify-center items-center mt-2 text-2xl font-bold mb-1">LOG IN</h1>
+        <hr  className="mb-4"/>
+      
+  
+
+      <label htmlFor="email">email</label>
+      <input
+        className="p-1 rounded-xl"
+        id="email"
+        type="text"
+        value={user.email}
+        onChange={(e) => {
+          setUser({ ...user, email: e.target.value });
+        }}
+        placeholder="email"
+      />
+
+      <label htmlFor="password">password</label>
+      <input
+          className="p-1 rounded-xl"
+        id="password"
+        type="text"
+        value={user.password}
+        onChange={(e) => {
+          setUser({ ...user, password: e.target.value });
+        }}
+        placeholder="password"
+      />
+
+      <div className="flex flex-col justify-center items-center gap-2 ">
+      <button className="p-2 bg-white rounded-xl w-24 mt-3 text-pink-600 font-bold" onClick={onLogIn}  >LOG IN</button>
+      <Link href="/signup">Visit signup page</Link>
+      </div>
+
+      
+
+        </div>
+      
     </div>
   );
 }
--- nextapp/src/app/signup/page.tsx
+++ nextapp/src/app/signup/page.tsx
@@ -12,11 +12,13 @@ export default function signUpPage() {
   const onSignUp = () => {};
   return (
     <div className="flex flex-col justify-center items-center h-screen gap-3">
-        <div className=" bg-pink-600 h-96 w-80 px-3">
-        <h1 className="">signUp</h1>
-      <hr className="flex flex-col gap-3" />
+        <div className=" bg-pink-600 h-96 w-80 px-3 flex flex-col gap-2">
+        <h1 className="flex justify-center items-center mt-2 text-2xl font-bold mb-1">SIGN UP</h1>
+        <hr  className="mb-4"/>
+      
       <label htmlFor="username">username</label>
       <input
+      className="p-1 rounded-xl"
         id="username"
         type="text"
         value={user.username}
@@ -28,6 +30,7 @@ export default function signUpPage() {
 
       <label htmlFor="email">email</label>
       <input
+        className="p-1 rounded-xl"
         id="email"
         type="text"
         value={user.email}
@@ -39,6 +42,7 @@ export default function signUpPage() {
 
       <label htmlFor="password">password</label>
       <input
+          className="p-1 rounded-xl"
         id="password"
         type="text"
         value={user.password}
@@ -47,6 +51,13 @@ export default function signUpPage() {
         }}
         placeholder="password"
       />
+
+      <div className="flex flex-col justify-center items-center gap-2 ">
+      <button className="p-2 bg-white rounded-xl w-24 mt-3 text-pink-600 font-bold" onClick={onSignUp}  >SIGN UP</button>
+      <Link href="/login">Visit login page</Link>
+      </div>
+
+      
 
         </div>
       
