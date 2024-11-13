d01ff76dd809dc32ba9b2ee93f0d05499caa4f6f
Cyiza2001 <82471629+Cyiza2001@users.noreply.github.com>
2024-11-13 22:24:15 +0200

    another commit

--- nextapp/src/app/signup/page.tsx
+++ nextapp/src/app/signup/page.tsx
@@ -1,13 +1,55 @@
-"use client"
-import Link from "next/link"
-import React from "react"
-import {useRouter} from "next/navigation"
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import { axios } from "axios";
+export default function signUpPage() {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+  const onSignUp = () => {};
+  return (
+    <div className="flex flex-col justify-center items-center h-screen gap-3">
+        <div className=" bg-pink-600 h-96 w-80 px-3">
+        <h1 className="">signUp</h1>
+      <hr className="flex flex-col gap-3" />
+      <label htmlFor="username">username</label>
+      <input
+        id="username"
+        type="text"
+        value={user.username}
+        onChange={(e) => {
+          setUser({ ...user, username: e.target.value });
+        }}
+        placeholder="username"
+      />
 
-export default function signUpPage (){
-    return(
-        <div>
-            <h1>signUp</h1>
+      <label htmlFor="email">email</label>
+      <input
+        id="email"
+        type="text"
+        value={user.email}
+        onChange={(e) => {
+          setUser({ ...user, email: e.target.value });
+        }}
+        placeholder="email"
+      />
+
+      <label htmlFor="password">password</label>
+      <input
+        id="password"
+        type="text"
+        value={user.password}
+        onChange={(e) => {
+          setUser({ ...user, password: e.target.value });
+        }}
+        placeholder="password"
+      />
+
         </div>
-    )
-
+      
+    </div>
+  );
 }
