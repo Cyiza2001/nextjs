eb622a514253f85f251b9be81dfa0d3a8e811086
Cyiza2001 <82471629+Cyiza2001@users.noreply.github.com>
2024-12-26 15:28:02 +0200

    another commit

---
+++ src/app/About/page.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <Head>
+        <title>About</title>
+      </Head>
+      <h1>about page</h1>
+    </div>
+  );
+};
+
+export default About;
